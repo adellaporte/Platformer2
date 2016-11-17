cc04b3745f70c8f3b439cc6b0b5f66864e05f7d5
adellaporte <adellaporte41@hawkmail.newpaltz.edu>
2016-11-17 18:13:37 -0500

    3 kinds of movement

---
+++ Assets/Scripts/PatrolMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrolMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    Vector3 direction = new Vector3(-1, -.1f);
+   
+    void Update()
+    {
+        
+        transform.position += direction * Time.deltaTime;
+
+        if (transform.position.x <= 0) {
+            direction = new Vector3(1, .1f);
+         }
+
+        if (transform.position.x >= 5) {
+            direction = new Vector3(-1, -.1f);
+        }
+        
+    }
+}
